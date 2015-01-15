2bbf04d5712e4764a4d5732ee3fdc227f0c45007
BBin <jjang9b@gmail.com>
2015-01-15 17:39:10 +0900

    Revert "[wiking] 0.0.1"
    This reverts commit c9cd572f41ab3e4bb8ced8ffbbe7adcfb922a0bc.

--- 1.php
+++ 1.php
@@ -32,18 +32,16 @@ function _setWrite()//{{{
   $aInfoAll[ $hash_name ] = $aInfo;
   $sInfo = _make('$info', $aInfoAll);
 
-  $result_tpl = file_put_contents($tpl_path, $conts);
+  $result_config = file_put_contents(CONFIG_PATH, '<?php'."\n".$sInfo); 
 
-  if($result_tpl)
+  if($result_config)
   {
+    @chmod(CONFIG_PATH, 0777);
+    $result_tpl = file_put_contents($tpl_path, $conts);
     @chmod($tpl_path, 0777);
 
-    $result_config = file_put_contents(CONFIG_PATH, '<?php'."\n".$sInfo); 
-
-    @chmod(CONFIG_PATH, 0777);
-
-    if($result_config)
-      _echo_json(array('hash_name'=>$hash_name, 'ret'=>$result_tpl.' | '.$result_config));
+    if($result_tpl)
+      _echo_json(array('hash_name'=>$hash_name, 'ret'=>$result_tpl));
   }
 
   _echo_json(array('hash_name'=>$hash_name, 'ret'=>false));
--- config/info.config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$info['a27309029a']['name']='sample';
-$info['a27309029a']['parent_hash']='';
-$info['a27309029a']['reg_dt']='20150114210339';
