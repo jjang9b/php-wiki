f476fbc7d59de01b4aa013e91cf4133e1ce26779
bbin <bbin@433.co.kr>
2015-01-16 11:10:22 +0900

    [wiking] menual tpl add

--- config/info.config.php
+++ config/info.config.php
@@ -1,4 +1,10 @@
 <?php
-$info['a27309029a']['name']='Javascript';
+$info['a27309029a']['name']='Start';
 $info['a27309029a']['parent_hash']='';
-$info['a27309029a']['reg_dt']='20150115175851';
+$info['a27309029a']['reg_dt']='20150116105403';
+$info['1342251a55']['name']='wiki introduce';
+$info['1342251a55']['parent_hash']='a27309029a';
+$info['1342251a55']['reg_dt']='20150116110923';
+$info['28592a0186']['name']='wiki tpl use case';
+$info['28592a0186']['parent_hash']='a27309029a';
+$info['28592a0186']['reg_dt']='20150116110741';
---
+++ tpl/28592a0186.tpl
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<span style="color: #993300;"><strong>source code highlighting</strong></span><br /><br />
+<pre><code>
+&lt;script type="text/javascript"&gt;<br />&nbsp; function hello(){<br />&nbsp; &nbsp; alert( 'hello, wiking' );<br />&nbsp; }<br />&lt;/script&gt;</code></pre>
+<pre><code class="php"><br />&lt;?php<br />&nbsp; @chmod( 'config', 0777);<br /><span>&nbsp; @chmod( 'tpl', 0777);</span><br />?&gt;<br /><br /></code></pre>
+<code> </code>
+</body>
+</html>
